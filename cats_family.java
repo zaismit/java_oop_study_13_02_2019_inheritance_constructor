bde253e0d0fb14a352b7d503a104d476f33be702
Alexander Zemskov <zaismit@gmail.com>
2019-02-16 18:37:48 +0200

    Add files via upload

---
+++ cats_family.java
@@ -0,0 +1,36 @@
+/**
+ * Created by ZAISMIT - EVYNET
+ **/
+public class cats_family
+{
+    private int ears;
+    private int legs;
+    private boolean big_claws;
+
+    public void setbig_claws(boolean big_claws)
+    {
+        this.big_claws = big_claws;
+    }
+
+    public cats_family()
+    {
+        ears = 2;
+        legs = 4;
+        big_claws = true;
+    }
+
+    public cats_family(int ears, int legs, boolean big_claws)
+    {
+        this.ears = ears;
+        this.legs = legs;
+        this.big_claws = big_claws;
+    }
+
+    public void show_info()
+    {
+        System.out.println( "qty ears: " + ears +
+                            " qty legs: " + legs +
+                            " big claws: " + big_claws);
+    }
+
+}
